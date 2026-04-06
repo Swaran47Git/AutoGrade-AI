a3808f2fe9377dae1f803d5db7adbebec1a257ef
Swaran47Git <Swaranpr47@gmail.com>
2026-04-06 13:08:46 +0530

    heehee6

--- DDS/my_admin/views.py
+++ DDS/my_admin/views.py
@@ -1,6 +1,6 @@
 import re, json
 import requests
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect,      
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
